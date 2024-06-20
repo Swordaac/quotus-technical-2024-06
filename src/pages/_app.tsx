1ece42af87e67900b2c94b466f20ce1ddf6f0da5
Jonathan Robinson <jonathan@quotus.com>
2024-06-20 09:33:34 -0400

    setting up project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import App from '@/components/pages/App'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function NextApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <App>
+        <Component {...pageProps} />
+      </App>
     </ChakraProvider>
   )
 }
