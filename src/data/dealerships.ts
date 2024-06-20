0d18f025ac664de1f1d8e353524c64dc3690150e
Jonathan Robinson <jonathan@quotus.com>
2024-06-20 09:45:27 -0400

    feat: set up dealership mock data

---
+++ src/data/dealerships.ts
@@ -0,0 +1,251 @@
+import type { Dealership } from "@/typescript/interfaces";
+
+export const dealershipData: { dealership: Dealership[] } = {
+    dealership: [
+        {
+            name: "Kia Yearly DEMO",
+            dealerCode: "CA-8374",
+            brand: null,
+            province: "QC",
+            country: "CA"
+        },
+        {
+            name: "GM Yearly DEMO",
+            dealerCode: "CA-8375",
+            brand: null,
+            province: "QC",
+            country: "CA"
+        },
+        {
+            name: "Hyundai Yearly DEMO",
+            dealerCode: "CA-8376",
+            brand: null,
+            province: "QC",
+            country: "CA"
+        },
+        {
+            name: "Ford Yearly DEMO",
+            dealerCode: "CA-8377",
+            brand: null,
+            province: "QC",
+            country: "CA"
+        },
+        {
+            name: "Volvo Demo",
+            dealerCode: "CA-8378",
+            brand: null,
+            province: "QC",
+            country: "CA"
+        },
+        {
+            name: "Chrysler Yearly DEMO",
+            dealerCode: "CA-8379",
+            brand: null,
+            province: "QC",
+            country: "CA"
+        },
+        {
+            name: "Chrysler 2 Yearly DEMO",
+            dealerCode: "CA-8380",
+            brand: null,
+            province: "QC",
+            country: "CA"
+        },
+        {
+            name: "Nissan DEMO",
+            dealerCode: "CA-8381",
+            brand: null,
+            province: "QC",
+            country: "CA"
+        },
+        {
+            name: "BMW DEMO",
+            dealerCode: "CA-8382",
+            brand: null,
+            province: "QC",
+            country: "CA"
+        },
+        {
+            name: "Mitsubishi DEMO",
+            dealerCode: "CA-8383",
+            brand: null,
+            province: "QC",
+            country: "CA"
+        },
+        {
+            name: "Stellantis DEMO",
+            dealerCode: "Q-CA-0001011",
+            brand: null,
+            province: "QC",
+            country: "CA"
+        },
+        {
+            name: "Stellantis DEMO 2",
+            dealerCode: "Q-CA-0001015",
+            brand: null,
+            province: "",
+            country: "US"
+        },
+        {
+            name: "Ford DEMO 2",
+            dealerCode: "Q-CA-0001016",
+            brand: null,
+            province: "QC",
+            country: "CA"
+        },
+        {
+            name: "Ford DEMO 3",
+            dealerCode: "Q-CA-0001017",
+            brand: null,
+            province: "QC",
+            country: "CA"
+        },
+        {
+            name: "Ford DEMO 4",
+            dealerCode: "Q-CA-0001018",
+            brand: null,
+            province: "QC",
+            country: "CA"
+        },
+        {
+            name: "Stellantis DEMO 3",
+            dealerCode: "Q-CA-0001019",
+            brand: null,
+            province: "ON",
+            country: "CA"
+        },
+        {
+            name: "Nissan Demo 1",
+            dealerCode: "Q-CA-0001028",
+            brand: null,
+            province: "QC",
+            country: "CA"
+        },
+        {
+            name: "Nissan Demo 2",
+            dealerCode: "Q-CA-0001029",
+            brand: null,
+            province: "QC",
+            country: "CA"
+        },
+        {
+            name: "Nissan Demo 3",
+            dealerCode: "Q-CA-0001030",
+            brand: null,
+            province: "QC",
+            country: "CA"
+        },
+        {
+            name: "Nissan Demo 4",
+            dealerCode: "Q-CA-0001031",
+            brand: null,
+            province: "QC",
+            country: "CA"
+        },
+        {
+            name: "Nissan Demo 5",
+            dealerCode: "Q-CA-0001032",
+            brand: null,
+            province: "",
+            country: "US"
+        },
+        {
+            name: "Nissan Demo 6",
+            dealerCode: "Q-CA-0001033",
+            brand: null,
+            province: "QC",
+            country: "CA"
+        },
+        {
+            name: "Nissan Demo 7",
+            dealerCode: "Q-CA-0001034",
+            brand: null,
+            province: "QC",
+            country: "CA"
+        },
+        {
+            name: "Nissan Demo 8",
+            dealerCode: "Q-CA-0001035",
+            brand: null,
+            province: "QC",
+            country: "CA"
+        },
+        {
+            name: "Nissan Demo 9",
+            dealerCode: "Q-CA-0001036",
+            brand: null,
+            province: "QC",
+            country: "CA"
+        },
+        {
+            name: "Nissan Demo 10",
+            dealerCode: "Q-CA-0001037",
+            brand: null,
+            province: "QC",
+            country: "CA"
+        },
+        {
+            name: "Ford DEMO",
+            dealerCode: "Q-CA-FORDDEMO",
+            brand: null,
+            province: "QC",
+            country: "CA"
+        },
+        {
+            name: "Mercedes DEMO",
+            dealerCode: "Q-DEMO-1001",
+            brand: null,
+            province: "QC",
+            country: "CA"
+        },
+        {
+            name: "Lexus DEMO",
+            dealerCode: "Q-DEMO-1002",
+            brand: null,
+            province: "QC",
+            country: "CA"
+        },
+        {
+            name: "Honda DEMO",
+            dealerCode: "Q-DEMO-1003",
+            brand: null,
+            province: "QC",
+            country: "CA"
+        },
+        {
+            name: "Toyota DEMO",
+            dealerCode: "Q-DEMO-1004",
+            brand: null,
+            province: "QC",
+            country: "CA"
+        },
+        {
+            name: "Hyundai DEMO",
+            dealerCode: "Q-DEMO-1005",
+            brand: null,
+            province: "QC",
+            country: "CA"
+        },
+        {
+            name: "Kia DEMO",
+            dealerCode: "Q-DEMO-1006",
+            brand: null,
+            province: "QC",
+            country: "CA"
+        },
+        {
+            name: "GMC DEMO",
+            dealerCode: "Q-DEMO-1007",
+            brand: null,
+            province: "QC",
+            country: "CA"
+        },
+        {
+            name: "Volskwagen DEMO",
+            dealerCode: "Q-DEMO-1008",
+            brand: null,
+            province: "QC",
+            country: "CA"
+        }
+    ]
+};
---
+++ src/typescript/interfaces.ts
@@ -0,0 +1,7 @@
+export interface Dealership {
+    name: string;
+    dealerCode: string;
+    brand: string | null;
+    province: string;
+    country: string;
+}
